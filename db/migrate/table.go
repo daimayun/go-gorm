95ed1759b626f504ce2724ef5c47c803639ed2f8
wangben <wangben-1203@qq.com>
2023-11-14 17:32:13 +0800

    ddl

--- db/migrate/table.go
+++ db/migrate/table.go
@@ -1,6 +1,7 @@
 package migrate
 
 import (
+	"errors"
 	"fmt"
 	"gorm.io/gorm"
 
@@ -34,6 +35,39 @@ func RenameTable(oldName, newName interface{}) error {
 
 func TableType(dst interface{}) (gorm.TableType, error) {
 	return db.DB.Migrator().TableType(dst)
+}
+
+// 获取表名
+func getTableName(dst interface{}) (tableName string, err error) {
+	v, ok := dst.(string)
+	if ok {
+		tableName = v
+		return
+	}
+	stmt := &gorm.Statement{DB: db.DB}
+	if err = stmt.Parse(dst); err == nil {
+		tableName = stmt.Table
+		return
+	}
+	return
+}
+
+func TableDDL(dst interface{}) (str string, err error) {
+	var tableName string
+	if tableName, err = getTableName(dst); err != nil {
+		return
+	}
+	var val map[string]interface{}
+	if err = db.DB.Raw(fmt.Sprintf("SHOW CREATE TABLE `%s`", tableName)).Scan(&val).Error; err != nil {
+		return
+	}
+	value, ok := val["Create Table"]
+	if ok {
+		str = value.(string)
+		return
+	}
+	err = errors.New("table [" + tableName + "] ddl not found")
+	return
 }
 
 type tableOption struct {
