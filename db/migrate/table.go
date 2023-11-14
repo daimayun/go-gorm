1f75216d5c9a4dfc316ffbbfee513f4a34abda58
wangben <wangben-1203@qq.com>
2023-11-14 10:48:13 +0800

    m

--- db/migrate/table.go
+++ db/migrate/table.go
@@ -2,12 +2,12 @@ package migrate
 
 import "github.com/daimayun/go-gorm/db"
 
-// GetTableList 获取表列表
+// GetTableList 获取数据库表列表
 func GetTableList() ([]string, error) {
 	return db.DB.Migrator().GetTables()
 }
 
-// HasTable 表是否存在 表名 string | model struct ptr
+// HasTable 表是否存在 [表名 string | model struct ptr]
 func HasTable(dst interface{}) bool {
 	return db.DB.Migrator().HasTable(dst)
 }
