1bae8416a5815d383d1f8dedf5173baa6665858d
wangben <wangben-1203@qq.com>
2023-11-23 14:31:06 +0800

    modify

--- db/migrate/column.go
+++ db/migrate/column.go
@@ -1,8 +1,24 @@
 package migrate
 
-import "github.com/daimayun/go-gorm/db"
+import (
+	"errors"
+
+	"github.com/daimayun/go-gorm/db"
+)
 
 // HasColumn 表字段是否存在
 func HasColumn(dst interface{}, field string) bool {
 	return db.DB.Migrator().HasColumn(dst, field)
+}
+
+func AddColumn(dst interface{}) (err error) {
+	return
+}
+
+func DeleteColumn(dst interface{}, field string) (err error) {
+	if !HasColumn(dst, field) {
+		err = errors.New("column [" + field + "] no exist")
+		return
+	}
+	return
 }
